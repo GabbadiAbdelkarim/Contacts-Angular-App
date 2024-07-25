2b937b437b27df8900ea5385c606f657fb6821ba
AGABBADI <agabbadi@omnilog.fr>
2024-07-25 10:11:51 +0200

    add post and get

--- UI/contacts.web/src/app/app.component.ts
+++ UI/contacts.web/src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../models/contact.model';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contacts.web';
+  http = inject(HttpClient);
+
+  contactsForm = new FormGroup({
+    name: new FormControl<string>(''),
+    email: new FormControl<string | null>(null),
+    phone : new FormControl<string>(''),
+    favorite: new FormControl<boolean>(false)
+  });
+  contacts$ = this.getContacts();
+
+  onFormSubmit() {
+    const addContactRequest = {
+      name: this.contactsForm.value.name,
+      email: this.contactsForm.value.email,
+      phone: this.contactsForm.value.phone,
+      favorite: this.contactsForm.value.favorite
+    }
+
+    this.http.post('https://localhost:7194/api/Contacts', addContactRequest)
+    .subscribe({
+      next : (value) => {
+        console.log(value);
+        this.contacts$ = this.getContacts();
+        this.contactsForm.reset();
+      }
+    });
+  }
+  private getContacts() : Observable<Contact[]>
+  {
+    return this.http.get<Contact[]>("https://localhost:7194/api/Contacts");
+  }
 }
